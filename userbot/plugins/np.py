79ac658f388feb7f412f78aa1b1935f05ae272bc
4rju9 <63835760+4rjun9@users.noreply.github.com>
2020-06-30 22:48:23 +0530

    Add files via upload

---
+++ userbot/plugins/np.py
@@ -0,0 +1,48 @@
+"""Emoji
+Available Commands:
+.np
+Credits to @pureindialover
+"""
+
+from telethon import events
+
+import asyncio
+
+from userbot.utils import admin_cmd
+
+@borg.on(admin_cmd("np"))
+async def _(event):
+    if event.fwd_from:
+        return
+    animation_interval = 0.2
+    animation_ttl = range(0,36)
+    #input_str = event.pattern_match.group(1)
+   # if input_str == "np":
+    await event.edit("np")
+    animation_chars = [
+            "😎",
+            "☠️",
+            "😈",
+            "Loading.",
+            "Loading..",
+            "Loading...",
+            "Loading....",
+            "Loading.....",
+            "𝐍𝐨....",
+            "𝐍𝐨 𝐨𝐧𝐞....",
+            "𝐍𝐨 𝐨𝐧𝐞 𝐜𝐚𝐧....",
+            "𝐍𝐨 𝐨𝐧𝐞 𝐜𝐚𝐧 𝐬𝐭𝐨𝐩....",
+            "𝐍𝐨 𝐨𝐧𝐞 𝐜𝐚𝐧 𝐬𝐭𝐨𝐩 𝐦𝐞....",
+            "𝐍𝐨 𝐨𝐧𝐞 𝐜𝐚𝐧 𝐬𝐭𝐨𝐩 𝐦𝐞 𝐍𝐨....",
+            "𝐍𝐨 𝐨𝐧𝐞 𝐜𝐚𝐧 𝐬𝐭𝐨𝐩 𝐦𝐞 𝐍𝐨 𝐨𝐧𝐞....",
+            "𝐍𝐨 𝐨𝐧𝐞 𝐜𝐚𝐧 𝐬𝐭𝐨𝐩 𝐦𝐞 𝐍𝐨 𝐨𝐧𝐞 𝐜𝐚𝐧....",
+            "𝐍𝐨 𝐨𝐧𝐞 𝐜𝐚𝐧 𝐬𝐭𝐨𝐩 𝐦𝐞 𝐍𝐨 𝐨𝐧𝐞 𝐜𝐚𝐧 𝐬𝐭𝐨𝐩....",
+            "𝐍𝐨 𝐨𝐧𝐞 𝐜𝐚𝐧 𝐬𝐭𝐨𝐩 𝐦𝐞 𝐍𝐨 𝐨𝐧𝐞 𝐜𝐚𝐧 𝐬𝐭𝐨𝐩 𝐭𝐡𝐞....",
+            "𝐍𝐨 𝐨𝐧𝐞 𝐜𝐚𝐧 𝐬𝐭𝐨𝐩 𝐦𝐞 𝐍𝐨 𝐨𝐧𝐞 𝐜𝐚𝐧 𝐬𝐭𝐨𝐩 𝐭𝐡𝐞 𝐟𝐮𝐭𝐮𝐫𝐞😈",
+         ]
+            
+
+    for i in animation_ttl:
+        	
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 18])
